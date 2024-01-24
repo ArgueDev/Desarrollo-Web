80cd128eef1cdc67e8d83d34791c67f4eb20d481
ArgueDev <christianarguelloo97@gmail.com>
2024-01-24 16:23:25 -0500

    Cambio de Cuenta

---
+++ IntroduccionPHP_inicio/02-variables.php
@@ -0,0 +1,17 @@
+<?php include 'includes/header.php';
+
+$nombre = "Christian";
+echo $nombre;
+var_dump($nombre);
+
+define("Constante", "Este es el valor");
+echo Constante;
+
+const constante2 = "Hola 2";
+echo constante2;
+
+$nombreCliente = "Pedro";
+$nombre_cliente = "Pedro";
+
+
+include 'includes/footer.php';
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Desarrollo-Web
+# Web_Developer
+Curso de Diseño web por Udemy, iniciado el 5 de octubre 2023
