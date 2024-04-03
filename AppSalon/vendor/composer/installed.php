221c7989694408eee5cf4ebd80e3960d199fab4d
ArgueDev <christianarguelloo97@gmail.com>
2024-04-02 23:57:23 -0500

    Continuacion Proyecto AppSalon - 2/Abril/2024

---
+++ AppSalon/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'user/app-salon',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1dccaa0be04efd70c75c347659376fedb23f9340',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user/app-salon' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1dccaa0be04efd70c75c347659376fedb23f9340',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ AppSalon/views/auth/login.php
@@ -0,0 +1,21 @@
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia Sesion con tus datos</p>
+
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Tu Email" name="email">
+    </div> <!-- Email -->
+    
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Tu Password" name="password">
+    </div> <!-- Password -->
+
+    <input type="submit" class="boton" value="Inicar Sesion">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aun no tienes una cuenta? Crea una...</a>
+    <a href="/olvide">¿Olvidaste tu Password?</a>
+</div>
