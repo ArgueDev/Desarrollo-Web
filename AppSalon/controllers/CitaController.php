82903714fc806cbda58c1dc1d8ebf6d627d4daa9
ArgueDev <christianarguelloo97@gmail.com>
2024-04-17 22:38:27 -0500

    17 - Abril - 2024

--- AppSalon/controllers/CitaController.php
+++ AppSalon/controllers/CitaController.php
@@ -4,12 +4,17 @@ namespace Controllers;
 
 use MVC\Router;
 
-class CitaController {
-    public static function index(Router $router) {
+class CitaController
+{
+    public static function index(Router $router)
+    {
         session_start();
-        
+
+        // Verificar si la clave 'nombre' existe en $_SESSION
+        $nombre = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : null;
+
         $router->render('cita/index', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $nombre
         ]);
     }
 }
--- AppSalon/src/js/app.js
+++ AppSalon/src/js/app.js
@@ -1,15 +1,43 @@
 let paso = 1;
+let pasoInical = 1;
+let pasoFinal = 3;
 
 document.addEventListener('DOMContentLoaded', function () {
     inicarApp();
 });
 
 function inicarApp() {
+    mostrarSeccion(); // Muestra y oculta las secciones
     tabs(); // Cambia la seccion cuando se presionen los tabs
+    botonPaginador(); // Agrega o quita los botones del paginador
+    paginaSiguiente();
+    paginaAnterior();
+    consultarAPI(); // Consulta la API en el backend de PHP
 }
 
 function mostrarSeccion() {
-    console.log('Mostrando');
+    // Ocultar la seccion que tenga la clase mostrar
+    const seccionAntior = document.querySelector('.mostrar');
+
+    if (seccionAntior) {
+        seccionAntior.classList.remove('mostrar');
+    }
+
+    // Seleccionar la seccion con el paso
+    const pasoSelector = `#paso-${paso}`;
+    const seccion = document.querySelector(pasoSelector);
+    seccion.classList.add('mostrar');
+
+    // Quita la clase actual al tab anterior
+    const tabAnterior = document.querySelector('.actual');
+
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+
+    // Resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
 
 function tabs() {
@@ -18,6 +46,82 @@ function tabs() {
         boton.addEventListener('click', function (e) {
             paso = parseInt(e.target.dataset.paso);
             mostrarSeccion();
+            botonPaginador();
         });
+    });
+}
+
+function botonPaginador() {
+    const paginaAnterior = document.querySelector('#anterior');
+    const paginaSiguiente = document.querySelector('#siguiente');
+
+    if (paso === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    } else if (paso == 3) {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.add('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
+
+    mostrarSeccion();
+}
+
+function paginaAnterior() { 
+    const paginaAnterior = document.querySelector('#anterior');
+    paginaAnterior.addEventListener('click', () => {
+        if (paso <= pasoInical) return;
+        paso--;
+        botonPaginador();
+    });
+}
+
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    paginaSiguiente.addEventListener('click', () => {
+        if (paso >= pasoFinal) return;
+        paso++;
+        botonPaginador();
+    });
+}
+
+async function consultarAPI() {
+
+    try {
+        const url = 'http://localhost:3000/api/servicios';
+        const resultado = await fetch(url);
+        const servicios = await resultado.json();
+        
+        mostrarServicios(servicios);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+function mostrarServicios(servicios) {
+    servicios.forEach(servicio => {
+        const { id, nombre, precio } = servicio;
+
+        const nombreServicio = document.createElement('P');
+        nombreServicio.classList.add('nombre-servicio');
+        nombreServicio.textContent = nombre;
+
+        const precioServicio = document.createElement('P');
+        precioServicio.classList.add('precio-servicio');
+        precioServicio.textContent = `$ ${precio}`;
+
+        const servicioDiv = document.createElement('DIV');
+        servicioDiv.classList.add('servicio');
+        servicioDiv.dataset.idServicio = id;
+
+        servicioDiv.appendChild(nombreServicio);
+        servicioDiv.appendChild(precioServicio);
+
+        document.querySelector('#servicios').appendChild(servicioDiv);
     });
 }
