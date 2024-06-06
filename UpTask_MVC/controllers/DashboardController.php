02635a5820f1c114e172af8cad511136c6620278
ArgueDev <christianarguelloo97@gmail.com>
2024-06-05 22:28:14 -0500

    Creando Proyectos desde el Dashboard

--- UpTask_MVC/controllers/DashboardController.php
+++ UpTask_MVC/controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
 use MVC\Router;
 
 class DashboardController {
@@ -18,11 +19,35 @@ class DashboardController {
 
     public static function crearProyecto(Router $router) {
         session_start();
-
         isAuth();
 
+        $alertas = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $proyecto = new Proyecto($_POST);
+            
+            /** Validacion */
+            $alertas = $proyecto->validarProyecto();
+
+            if (empty($alertas)) {
+                /** Generar un url unica */
+                $hash = md5(uniqid());
+                $proyecto->url = $hash;
+
+                /** Almacenar el creador del proyecto */
+                $proyecto->propietario_id = $_SESSION['id'];
+
+                /** Guardar Proyecto */
+                $proyecto->guardar();
+
+                /** Redireccionar */
+                header('location: /proyecto?url=' . $proyecto->url);
+            }
+        }
+
         $router->render('dashboard/crear-proyecto', [
-            'titulo' => 'Crear Proyecto'
+            'titulo' => 'Crear Proyecto',
+            'alertas' => $alertas
         ]);
     }
 
