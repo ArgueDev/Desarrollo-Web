fb88a3eae5b26db60d8fb43c470c232de6e6c0ea
ArgueDev <christianarguelloo97@gmail.com>
2024-06-11 21:58:12 -0500

    Formulario para agregar tareas en un modal

---
+++ UpTask_MVC/src/js/tarea.js
@@ -0,0 +1,45 @@
+(function () {
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML =
+            `
+                <form class="formulario nueva-tarea">
+                    <legend>Agrega una nueva tarea</legend>
+                    <div class="campo">
+                        <label>Tarea</label>
+                        <input type="text" name="tarea" placeholder="Agregar tarea al proyecto actual" id="tarea"/>
+                    </div>
+                    <div class="opciones">
+                        <input type="submit" class="submit-nueva-tarea" value="Agregar nueva tarea" />
+                        <button type="button" class="cerrar-modal">Cancelar</button>
+                    </div>
+                </form>
+            `;
+        
+        setTimeout(() => {
+            const formulario = document.querySelector('.formulario');
+            formulario.classList.add('animar');
+        }, 0);
+
+        modal.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            if (e.target.classList.contains('cerrar-modal')) {
+                const formulario = document.querySelector('.formulario');
+                formulario.classList.add('cerrar');
+
+                setTimeout(() => {
+                    modal.remove();
+                }, 500);
+            }
+        });
+        
+        document.querySelector('body').appendChild(modal);
+    }
+
+})();
